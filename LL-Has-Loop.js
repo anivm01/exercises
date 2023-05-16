8d6375e737cae7bfbf1f559082c3074a33a5ee00
anivm01 <anivm.webdev@gmail.com>
2023-05-16 19:48:37 -0400

    has loop exercise using the tortoise and hare algorithm

--- LL-Has-Loop.js
+++ LL-Has-Loop.js
@@ -60,10 +60,18 @@ class LinkedList {
   }
 
   // WRITE HAS LOOP METHOD HERE //
-  //                            //
-  //                            //
-  //                            //
-  //                            //
+  hasLoop() {
+    let slow = this.head;
+    let fast = this.head;
+    while (fast !== null && fast.next !== null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow === fast) {
+        return true;
+      }
+    }
+    return false;
+  }
   ////////////////////////////////
 }
 
